d8238c04547057bb6aed1ef047adb062e9157492
Mnabil10 <nabilm604@gmail.com>
2022-11-11 01:43:32 +0200

    add route in api

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,12 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function clubs(){
-    return $this->belongsToMany(
-        Club::class,
-        'favourites',
-        'user_id',
-        'club_id');
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
 
 Route::get('leagues', [LeaguesController::class,'index']);
 
