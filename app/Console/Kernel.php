e05b4a8c6fd7c3e09cf96eea6ca3b71384d5ecf5
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-11 21:27:15 +0200

    added jobs to seed Database

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 
+use App\Jobs\Leagues;
+use App\Jobs\Leagues_Scorers;
+use App\Jobs\PL;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +18,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // $schedule->job(new Leagues());
+        $schedule->job(new Leagues_Scorers());
     }
 
     /**
