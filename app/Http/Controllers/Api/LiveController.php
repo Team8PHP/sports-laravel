b68b18031032668a5c0e82183d887c120f661856
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-14 02:46:47 +0200

    update querry condition error

--- app/Http/Controllers/Api/LiveController.php
+++ app/Http/Controllers/Api/LiveController.php
@@ -12,25 +12,24 @@ class LiveController extends Controller
 {
     public function index()
     {
-
-        $matches = Matches::where('status','live')->get();
-        foreach($matches as $match ){
-            $homeid = $match->value('home_id');
-            $home = Club::find( $homeid);
+        $matches = Matches::where('status', 'live')->get();
+        foreach ($matches as $match) {
+            $homeid = $match->home_id;
+            $home = Club::find($homeid);
             $match->homeClub = $home;
-            $awayid = $match->value('away_id');
-            $away = Club::find( $awayid);
+            $awayid = $match->away_id;
+            $away = Club::find($awayid);
             $match->awayClub = $away;
-            $compid = $match->value('comp_id');
-            $comp = Competetion::find( $compid);
+            $compid = $match->comp_id;
+            $comp = Competetion::find($compid);
             $match->comp = $comp;
         }
-        
+
         return [
         'match'=>$matches
         // ,'homeClub'=>$match->homeClub
         // ,'awayclub'=>$match->awayClub
         // ,'comp'=>$match->comp
     ];
-}
+    }
 }
