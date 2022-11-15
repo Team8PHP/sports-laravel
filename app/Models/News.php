51ed66ddf756c3834b5b2b73d735f6f1715b7724
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-15 04:43:00 +0200

    create news model

---
+++ app/Models/News.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'source',
+        'author',
+        'title',
+        'description',
+        'content',
+        'date',
+        'url',
+        'urlToImage',
+        'time',
+    ];
+}
