5e530f58e00e128023b78386cab48b89984ab90b
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-16 20:11:35 +0200

    remove blueprint

--- database/migrations/2022_11_15_204035_add_role_to_users_table.php
+++ database/migrations/2022_11_15_204035_add_role_to_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,8 +12,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role',['user','admin'])->default('user');
+        Schema::table('users', function ($table) {
+            $table->enum('role', ['user','admin'])->default('user');
         });
     }
 
@@ -25,7 +24,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', function ($table) {
             $table->dropColumn('role');
         });
     }
