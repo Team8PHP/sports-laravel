7c4f2c94d5dfd9741766024060bd0d8ab93f1697
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-16 18:56:49 +0200

    remove duplicate enum

--- database/migrations/2022_11_08_091940_create_matches_table.php
+++ database/migrations/2022_11_08_091940_create_matches_table.php
@@ -29,7 +29,7 @@ return new class () extends Migration {
                 'REGULAR_SEASON','FINAL','THIRD_PLACE' ,'SEMI_FINALS' ,'QUARTER_FINALS' ,'LAST_16' ,'LAST_32' ,'LAST_64' ,
                 'ROUND_4','ROUND_3' , 'ROUND_2' , 'ROUND_1' , 'GROUP_STAGE' , 'PRELIMINARY_ROUND' , 'QUALIFICATION' ,
                 'QUALIFICATION_ROUND_1' , 'QUALIFICATION_ROUND_2' , 'QUALIFICATION_ROUND_3' , 'PLAYOFF_ROUND_1' , 'PLAYOFF_ROUND_2' ,
-                 'PLAYOFFS' , 'REGULAR_SEASON' , 'CLAUSURA' , 'APERTURA' , 'CHAMPIONSHIP' , 'RELEGATION' , 'RELEGATION_ROUND',
+                 'PLAYOFFS', 'CLAUSURA' , 'APERTURA' , 'CHAMPIONSHIP' , 'RELEGATION' , 'RELEGATION_ROUND',
             ]);
             $table->date('date');
             $table->string('time');
