47106e34a28e6ab928f0662e20038043e188c2fb
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-18 19:40:12 +0200

    add country image and names to fillable

--- app/Models/Club.php
+++ app/Models/Club.php
@@ -15,6 +15,8 @@ class Club extends Model
         'tla',
         'venue',
         'founded',
+        'country_image',
+        'country_name'
     ];
 
     // public function favourites()
@@ -34,7 +36,7 @@ class Club extends Model
 
     public function players()
     {
-        return $this->hasMany(Player::class,'club_id','id');
+        return $this->hasMany(Player::class, 'club_id', 'id');
     }
 
     public function users()
