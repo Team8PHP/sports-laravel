0f53a9a2b7151023746d502d0701baf15f8c9ed1
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-18 17:10:51 +0200

    add task to update league standing and scorers

---
+++ app/Jobs/UpdateLeagueScorersStanding.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateLeagueScorersStanding implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $leagues=['PL','SA','BL1','PD','FL1'];
+        $date = date('Y-m-d');
+        foreach ($leagues as $index=>$league) {
+            seed_league_standings($league);
+            seed_league_scorer($league);
+        }
+        sleep(60);
+    }
+}
