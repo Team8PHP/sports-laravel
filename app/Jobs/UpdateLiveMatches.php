19f74b79caa12187c457366f289c8d3d4f040cbc
Mohamed Taha <mohamedmtaha30@gmail.com>
2022-11-18 17:47:19 +0200

    add job to update live matches

---
+++ app/Jobs/UpdateLiveMatches.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateLiveMatches implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $leagues=['PL','SA','BL1','PD','FL1'];
+        $date = date('Y-m-d');
+        foreach ($leagues as $league) {
+            seed_matches($league, $date, $date);
+        }
+        sleep(30);
+    }
+}
